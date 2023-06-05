faa7c31a17ae4fd3edf5ca58f71b007d3a555f2c
csmedeiros <caio.s.medeiros@outlook.com>
2023-06-05 06:33:47 -0300

    05/06/2023 06:33

--- app/src/main/java/com/example/ap2/MainActivity.kt
+++ app/src/main/java/com/example/ap2/MainActivity.kt
@@ -1,45 +1,71 @@
 package com.example.ap2
 
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private var isIdadeFirstClick = true
     private var isNomeFirstClick = true
+    private var isMatriculaFirstClick = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val pesquisa = findViewById<Button>(R.id.btnPesquisa)
 
-        pesquisa.setOnClickListener {
-            val intent = Intent(this, Pesquisa1::class.java)
-            startActivity(intent)
-        }
+        val pesquisa = findViewById<Button>(R.id.btnPesquisa)
         val nome = findViewById<EditText>(R.id.etNome)
         val idade = findViewById<EditText>(R.id.etIdade)
+        val matricula = findViewById<EditText>(R.id.etMatricula)
+        val erro = findViewById<TextView>(R.id.tvErroNomeIdade)
 
         nome.inputType = android.text.InputType.TYPE_CLASS_TEXT
         idade.inputType = android.text.InputType.TYPE_CLASS_NUMBER
+        matricula.inputType = android.text.InputType.TYPE_CLASS_NUMBER
+        erro.visibility = View.INVISIBLE
+        erro.setTextColor(Color.RED)
 
         nome.setOnFocusChangeListener { _, hasFocus ->
-            if(hasFocus and nome.text.contains("Nome")) {
+            if (hasFocus) {
                 nome.text.clear()
-            }
-            else {
+            } else if (!hasFocus && nome.text.isEmpty()) {
                 nome.setText("Nome")
             }
         }
 
         idade.setOnFocusChangeListener { _, hasFocus ->
-            if(hasFocus and idade.text.contains("Idade")) {
+            if (hasFocus and isIdadeFirstClick) {
                 idade.text.clear()
-            }
-            else {
+            } else if (!hasFocus && idade.text.isEmpty()) {
                 idade.setText("Idade")
             }
         }
+        matricula.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus and isMatriculaFirstClick) {
+                matricula.text.clear()
+            } else if (!hasFocus && matricula.text.isEmpty()) {
+                matricula.setText("Matrícula")
             }
         }
+
+        pesquisa.setOnClickListener {
+                if (nome.text.isNotEmpty() && !nome.text.contentEquals("Insira aqui seu nome") and idade.text.isNotEmpty()
+                    and !idade.text.contentEquals("Insira aqui sua idade") and matricula.text.isNotEmpty()   and !matricula.text.contentEquals("Insira aqui sua matrícula")) {
+                val bundle = Bundle()
+                bundle.putString("nome", nome.text.toString())
+                bundle.putInt("idade", idade.text.toString().toInt())
+
+                val intent = Intent(this, Pesquisa1::class.java)
+                intent.putExtras(bundle)
+                startActivity(intent)
+            } else {
+                erro.visibility = View.VISIBLE
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/ap2/Pesquisa1.kt
+++ app/src/main/java/com/example/ap2/Pesquisa1.kt
@@ -2,13 +2,27 @@ package com.example.ap2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class Pesquisa1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pesquisa1)
 
+        val extras = intent.extras
 
+        fun recuperaDados():List<String> {
+                val nome = extras?.getString("nome")
+                val idade = extras?.getInt("idade")
+                // Faça o que deseja com os dados recuperados
+                // por exemplo, exiba-os em uma TextView ou realize alguma lógica com eles
+                val dados: List<String> = listOf(nome.toString(), idade.toString())
+                return dados
+        }
 
+        val recepcaoStr = "Olá " + recuperaDados()[0] + ", seja bem vindo!\n Vamos começar seu teste..."
+        val recepcao = findViewById<TextView>(R.id.tvRecepcao)
+
+        recepcao.setText(recepcaoStr)
     }
 }
